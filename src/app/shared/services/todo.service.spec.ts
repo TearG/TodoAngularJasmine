2f3b74f6fd1eede005205a0fba20d655969296de
Van Faria <57229209+TearG@users.noreply.github.com>
2023-02-16 00:35:38 +0000

    Parei em 10 minutos do segundo vídeo.

--- src/app/shared/services/todo.service.spec.ts
+++ src/app/shared/services/todo.service.spec.ts
@@ -1,12 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { TodoService } from './todo.service';
+
+
 
 fdescribe('TodoService', () => {
   let service: TodoService;
 
+  const httpStub = {
+    get: (_params: any) => of([
+      {
+      "userId": 1,
+      "id": 1,
+      "title": 'delectus aut autem',
+      "completed": false
+      }]
+    )
+  } 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: httpStub
+        }
+      ]
+    });
     service = TestBed.inject(TodoService);
   });
 
